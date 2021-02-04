0c2dd437563bcee691a655a423147d8c72533ac6
Marcel Dierkes <marcel@dierkes.co>
2021-02-04 19:42:08 +0100

    Added a DTILayoutElementValueTransformer

---
+++ DTIPreferences/DTILayoutPreferencesViewController/DTILayoutElementValueTransformer.h
@@ -0,0 +1,17 @@
+//
+//  DTILayoutElementValueTransformer.h
+//  DesktopTimeKit
+//
+//  Created by Marcel Dierkes on 04.02.21.
+//
+
+#import <Foundation/Foundation.h>
+#import <DesktopTimeKit/DesktopTimeKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTILayoutElementValueTransformer : NSValueTransformer
+
+@end
+
+NS_ASSUME_NONNULL_END
